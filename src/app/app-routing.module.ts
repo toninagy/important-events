faa95377007dacb3f099fdde0075972e50e18074
toninagy <sm4le99tony@gmail.com>
2022-05-13 16:02:28 +0200

    Add base components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventDetailComponent } from './components/event-detail/event-detail.component';
+import { EventFormComponent } from './components/event-form/event-form.component';
+import { EventListComponent } from './components/event-list/event-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/events",
+    pathMatch: "full",
+  },
+  {
+    path: "events",
+    component: EventListComponent,
+  },
+  {
+    path: "events/new",
+    component: EventFormComponent,
+  },
+  {
+    path: "events/:id",
+    component: EventDetailComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { EventListComponent } from './components/event-list/event-list.component';
+import { EventDetailComponent } from './components/event-detail/event-detail.component';
+import { EventFormComponent } from './components/event-form/event-form.component';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent,
+    EventDetailComponent,
+    EventFormComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
