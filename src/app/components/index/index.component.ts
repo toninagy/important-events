50b4bd75a41590ed8deaf41c7888298c3a0113d8
toninagy <sm4le99tony@gmail.com>
2022-05-27 22:44:46 +0200

    Fix index component event numbers and dates

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from 'src/app/event.service';
 
 @Component({
   selector: 'app-index',
@@ -7,13 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  // public num: number = 0 ;
   public numObj = {
-    counter:0,
-    dates:""
+    counter: 0,
+    dates: ["", ""]
   }
 
-  constructor() { }
+  constructor(private eventService: EventService) {
+    eventService.getAll().subscribe(val => {
+      this.numObj.counter = val.length;
+
+      var earliestDate = val[0].date;
+      var latestDate = val[0].date;
+
+      val.forEach(function (value) {
+        if(value.date < earliestDate) {
+          earliestDate = value.date;
+        }
+        if(value.date > latestDate) {
+          latestDate = value.date;
+        }
+      });
+
+      this.numObj.dates[0] = earliestDate;
+      this.numObj.dates[1] = latestDate;
+    });
+  }
 
   ngOnInit(): void {
   }
